1ba06f1f92add85456c085b8fa5bdd14d67559db
Daniel Salgado <danielsalgado.rom@gmail.com>
2022-10-26 19:00:11 -0600

    Products Layout Added

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Product = () => {
+import { urlFor } from '../lib/client'
+
+const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
-      Product
+      <Link href={`/pproduct/${slug.current}`}>
+        <div className='product-card'>
+          <img 
+            src={urlFor(image && image[0])} 
+            width={250}
+            height={250}
+            className='product-image'
+            alt="" 
+          />
+          <p className='product-name'>
+            {name}
+          </p>
+          <p className='product-price'>
+            ${price}
+          </p>
+        </div>
+      </Link>
     </div>
   )
 }
