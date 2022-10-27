e5c84a3472ce74c932cbe9d6d7cd374d741265cc
Daniel Salgado <danielsalgado.rom@gmail.com>
2022-10-27 14:59:17 -0600

    Use context logic with global states ready

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react'
-import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
-import { Product } from '../../components'
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from '../../components';
 
-import { client, urlFor } from '../../lib/client'
+import { client, urlFor } from '../../lib/client';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ( {product, products}) => {
 
   const { image, name, details, price } = product;
   const [ index, setIndex] = useState(0)
+  const { decQty, incQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -50,14 +52,14 @@ const ProductDetails = ( {product, products}) => {
             <div className='quantity'>
               <h3>Quantity:</h3>
               <p className='quantity-desc'>
-                  <span className='minus' onClick=''><AiOutlineMinus /></span>
-                  <span className='num' onClick=''> 0 </span>
-                  <span className='plus' onClick=''><AiOutlinePlus /></span>
+                  <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+                  <span className='num' onClick=''> {qty} </span>
+                  <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
               </p>
             </div>
 
             <div className='buttons'>
-              <button type='button' className='add-to-cart' onClick=''>Add to cart</button>
+              <button type='button' className='add-to-cart' onClick={ () => onAdd(product, qty)}>Add to cart</button>
               <button type='button' className='buy-now' onClick=''>Buy Now</button>
             </div>
           </div>
