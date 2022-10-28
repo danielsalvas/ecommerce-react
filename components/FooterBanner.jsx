6d4a3e43ed636dd61b3a7414866dbbd954cd52f4
Daniel Salgado <danielsalgado.rom@gmail.com>
2022-10-27 21:43:34 -0600

    Responsive design fixed

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -10,13 +10,10 @@ const FooterBanner = ( { footerBanner: { desc, discount, smallText, largeText1, 
         <div className='left'>
           <p>{discount}</p>
           <h3>{largeText1}</h3>
-          <h3>{largeText2}</h3>
           <p>{saleTime}</p>
         </div>
         <div className='right'>
-          <p>{smallText}</p>
           <h3>{midText}</h3>
-          <p>{desc}</p>
           <Link href={`/product/${product}`}>
             <button
               type='button'
